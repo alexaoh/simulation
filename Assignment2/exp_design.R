94b73e85d1a791d5344c12cfaddf85919cd5973d
alexaoh <alexaoh@users.noreply.github.com>
2022-05-27 19:39:53 +0200

    Continued working on Assignment 2.

---
+++ Assignment2/exp_design.R
@@ -0,0 +1,28 @@
+library(FrF2)
+?gl
+
+ExperimentDesign <- expand.grid(A = gl(2, 1, labels = c("-", "+")),
+                                B = gl(2, 1, labels = c("-", "+")),
+                                C = gl(2, 1, labels = c("-", "+")),
+                                D = gl(2, 1, labels = c("-", "+")))
+ExperimentDesign
+
+write.csv(ExperimentDesign, "design.csv", row.names = F)
+
+y <- c(0.995, 0.98, 0.975, 0.97,
+       0.98, 0.96, 0.94, 0.95,
+       0.98, 0.97, 0.94, 0.91,
+       0.96, 0.91, 0.93, 0.88)
+y <- rev(y)
+ep <- cbind(ExperimentDesign, y)
+ep
+
+write.csv(ep, "design.csv", row.names = F)
+
+library(unrepx)
+yates(y)
+
+fit <- lm(formula = y ~ (A + B + C + D)^3, data = ExperimentDesign)
+summary(fit)
+par(mfrow=c(2,2))
+plot(fit)
